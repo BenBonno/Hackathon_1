ff3e723cd74bc7ad47b5e0dfb8b9cb89245c59a4
BenBonno <benoit.bonnoron@gmail.com>
2022-11-25 09:00:14 +0100

    card ok missing style

--- hackathonWCS/src/App.jsx
+++ hackathonWCS/src/App.jsx
@@ -1,26 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import CityCard from "./components/CityCard";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <BrowserRouter>
-      <header className="w-full">
-        <NavBar />
-      </header>
-      <main className="">
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/home" exact element={<Home />} />
-          <Route path="/favorite" exact element={<Favorite />} />
-        </Routes>
-      </main>
-      <footer className="">
-        <Footer />
-      </footer>
-    </BrowserRouter>
+    <div>
+      <CityCard/>
+    </div>
   );
 }
 
--- hackathonWCS/src/components/CityCard.jsx
+++ hackathonWCS/src/components/CityCard.jsx
@@ -1,27 +1,57 @@
 import React from "react";
+import covid from "../assets/covidLogo.svg";
+import bgCardImage from "../assets/bgCardImage.svg";
+import cardImg from "../assets/cadreImg.svg";
+import trombone from "../assets/trombone.svg";
 
 function CityCard() {
+  
   return (
-    <div>
-      <a
-        href="#"
-        classname="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-      >
-        <img
-          classname="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-          src="/docs/images/blog/image-4.jpg"
-          alt=""
-        />
-        <div classname="flex flex-col justify-between p-4 leading-normal">
-          <h5 classname="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
-          </h5>
-          <p classname="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
-        </div>
-      </a>
+    <div className="flex justify-center">
+      <div className="flex items-center gap-3 border-2 border-[#FE9063] rounded-3xl h-[30vh] w-11/12 ">
+        <section className=" h-full w-3/4">
+          <div className="flex flex-col ">
+            <div className="flex justify-evenly p-2 ">
+              <img src={covid} alt=" covidLogo" />
+              <p className="">covid area</p>
+            </div>
+            <div className="flex justify-center">
+              <img
+                className="object-cover bg-red-600 h-full w-5/6 rounded-3xl "
+                src={bgCardImage}
+                alt=""
+              />
+            </div>
+          </div>
+        </section>
+        <section className="w-3/5 flex flex-col">
+          <div className="flex gap-6">
+            <div className="">
+              <span className="text-xl text-orange-400">&#9733;</span>
+              <span className="text-xl text-orange-400">&#9733;</span>
+              <span className="text-xl text-orange-400">&#9733;</span>
+              <span className="text-xl text-orange-400">&#9733;</span>
+              <span className="text-xl text-grey-200">&#9733;</span>
+            </div>
+            <div>
+            <img className="h-[4vh]" src={trombone} alt="trombone" />
+            </div>
+          </div>
+          <div className="flex flex-col pb-10 ">
+            <div className="text-red-200 ">info 1</div>
+            <div className="text-blue-200 ">info 2</div>
+            <div className=" text-blue-800 ">info 3</div>
+          </div>
+          <div className="flex justify-center">
+            <button
+              type="button"
+              className="text-white bg-[#27C7D4] border-[#32AFB9] border-2  font-medium rounded-3xl text-sm py-2.5 w-5/6 text-center flex-start"
+            >
+              Show more
+            </button>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
